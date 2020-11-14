f80796d0dabdc2a2c700f81d8973960d936d1e5a
Monsterope <opelove606@gmail.com>
2020-11-14 10:55:27 +0700

    refactor using strconv

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,17 +1,15 @@
 package fizzbuzz
 
+import (
+	"strconv"
+)
+
 func String(n int) string {
 	if n == 5 {
 		return "Buzz"
 	}
-	if n == 4 {
-		return "4"
-	}
 	if n == 3 {
 		return "Fizz"
 	}
-	if n == 2 {
-		return "2"
-	}
-	return "1"
+	return strconv.Itoa(n)
 }
